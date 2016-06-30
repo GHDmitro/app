77b64a080a18f29194778ea7d27bc0214226cfdc
GHDmitro <d.timosh1996@gmail.comd.timosh1996@gmail.comd.timosh1996@gmail.com>
2016-06-30 11:49:49 +0300

    Test

--- src/main/java/pac/controller/MyController.java
+++ src/main/java/pac/controller/MyController.java
@@ -43,7 +43,7 @@ public class MyController {
     private BookingService bookingService;
     @Autowired
     private BookingPositionService bookingPositionService;
-    private String pathToImg = "/App/src/main/webapp/images/";
+    private String pathToImg = "../pages/imagesUsers/";
 
 //    private ContactService contactService;
 
