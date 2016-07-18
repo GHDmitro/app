e19acc3e9defa52c04abc369fc64103eabc2e1c0
GHDmitro <d.timosh1996@gmail.comd.timosh1996@gmail.comd.timosh1996@gmail.com>
2016-07-18 13:52:14 +0300

    Test1

--- src/main/java/pac/controller/MyController.java
+++ src/main/java/pac/controller/MyController.java
@@ -239,7 +239,7 @@ public class MyController {
             String login = userDetail.getUsername();
             Account account = accountService.findAccount(login);
 
-            String ref = name+codeOfModel + login +IMAGE_EXTENSION; //
+            String ref = name+codeOfModel + login ; // IMAGE_EXTENSION
 
 //        String relativepath = "/img/";
 //        String absolutePath = request.getRealPath(relativepath);
@@ -277,7 +277,7 @@ public class MyController {
                     }
 //                    System.out.println(file.getAbsolutePath());
 //
-                    File file1 = new File(file, ref);
+                    File file1 = new File(file, ref +IMAGE_EXTENSION);
 
                     try {
                         FileOutputStream fileOut = new FileOutputStream(file1);
